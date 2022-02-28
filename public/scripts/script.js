801c702ec3d44d0972618602a03db712e379b16d
Isaac Baldivas <ice.baldivas@gmail.com>
2022-02-28 10:10:27 -0600

    Compare Tiles for Bingo

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,9 +3,15 @@ $(document).ready(() => {
 
 	// Click Function
 	$('.tile').click((e) => {
-		if ($(e.currentTarget).css('background').includes('rgb(241, 239, 100)'))
-			$(e.currentTarget).css('background', '#3c0058');
-		else $(e.currentTarget).css('background', '#f1ef64');
+		if ($(e.currentTarget).css('background').includes('rgb(241, 239, 100)')) {
+			$(e.currentTarget).css('background', '#3c0058').css('color', '#fff');
+			$(e.currentTarget).attr('data-checked', 'uncheck');
+			checkForBingo();
+		} else {
+			$(e.currentTarget).css('background', '#f1ef64').css('color', '#272727');
+			$(e.currentTarget).attr('data-checked', 'check');
+			checkForBingo();
+		}
 	});
 
 	renderItems();
@@ -33,12 +39,59 @@ async function renderItems() {
 		items.splice(randomIndex, 1);
 	}
 
-	console.table(order);
+	// console.table(order);
+
 	for (let i = 0; i < order.length; i++) {
 		if (i == 12) {
 			$($('.tile-text')[i]).html('Free Space');
 		} else $($('.tile-text')[i]).html(order[i].name);
 	}
+}
+
+function checkForBingo() {
+	let bingo = false;
+	const correctOrders = [
+		// Rows
+		[0, 1, 2, 3, 4],
+		[5, 6, 7, 8, 9],
+		[10, 11, 12, 13, 14],
+		[15, 16, 17, 18, 19],
+		[20, 21, 22, 23, 24],
+		// Columns
+		[0, 5, 10, 15, 20],
+		[1, 6, 11, 16, 21],
+		[2, 7, 12, 17, 22],
+		[3, 8, 13, 18, 23],
+		[4, 9, 14, 19, 24],
+		// Diagonal
+		[0, 6, 12, 18, 24],
+		[4, 8, 12, 16, 20],
+	];
+
+	function compareOrder() {
+		let compared = false;
+		for (let i = 0; i < correctOrders.length; i++) {
+			// CorrectOrders Loop
+			let trueCounter = 0;
+			for (let j = 0; j < correctOrders[i].length; j++) {
+				// Specific Order Loop
+				let comparedIndex = correctOrders[i][j];
+				if (indexes.includes(comparedIndex)) trueCounter++;
+			}
+			if (trueCounter == correctOrders[i].length) compared = true;
+		}
+		return compared;
+	}
+
+	let checked = Array.from($('[data-checked="check"]'));
+	let indexes = [];
+	checked.forEach((element) => {
+		index = $(element).index();
+		if (!indexes.includes(index)) indexes.push(index);
+	});
+	indexes.sort((a, b) => a - b);
+	console.log(indexes);
+	console.log(`Compared Order: ${compareOrder()}`);
 }
 
 /**
